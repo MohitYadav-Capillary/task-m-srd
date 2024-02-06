4e5eac28aded6b9b79e2dcf8a690cf7dd997833d
MohitYadav-Capillary <mohit.yadav@capillarytech.com>
2024-02-06 15:34:39 +0530

    commit-3

--- testCases.test.js
+++ testCases.test.js
@@ -12,3 +12,11 @@ it.todo("when create task page is rendered, it should have the form elements");
 it.todo(
   "when the user submits the form, it should create the task and navigate to the home page"
 );
+
+//
+it.todo(
+  "when the user clicks the task name, it should navigate to the view and edit task page"
+);
+it.todo(
+  "when the user clicks the delete icon, it should delete the task and update the list"
+);
