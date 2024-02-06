11105b391c88df877c785effe13eb9963d9b4347
MohitYadav-Capillary <mohit.yadav@capillarytech.com>
2024-02-06 15:34:03 +0530

    commit-2

--- testCases.test.js
+++ testCases.test.js
@@ -2,3 +2,13 @@ it.todo(
   "should render the Home page with the initial tasks and static elements"
 );
 it.todo("when the user filters the tasks, it should render the filtered tasks");
+
+//
+
+it.todo(
+  "when the user clicks the create task button, it should navigate to the create task page"
+);
+it.todo("when create task page is rendered, it should have the form elements");
+it.todo(
+  "when the user submits the form, it should create the task and navigate to the home page"
+);
